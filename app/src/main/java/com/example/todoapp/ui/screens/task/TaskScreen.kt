c76f539dc4aabdfe17c28ba37aae908af80b2b10
Ben Haas <ben@benhaas.io>
2024-08-07 11:57:10 -0700

    add toast for empty field validation

--- app/src/main/java/com/example/todoapp/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/example/todoapp/ui/screens/task/TaskScreen.kt
@@ -1,5 +1,7 @@
 package com.example.todoapp.ui.screens.task
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -7,11 +9,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import com.example.todoapp.data.models.Priority
 import com.example.todoapp.data.models.ToDoTask
-import com.example.todoapp.ui.screens.list.ListContent
 import com.example.todoapp.ui.viewmodels.SharedViewModel
 import com.example.todoapp.util.Action
+import com.example.todoapp.R
 
 @Composable
 fun TaskScreen(
@@ -23,11 +27,23 @@ fun TaskScreen(
     val description: String by sharedViewModel.description
     val priority: Priority by sharedViewModel.priority
 
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
             TaskAppBar(
                 selectedTask = selectedTask,
-                navigateToListScreen = navigateToListScreen
+                navigateToListScreen = { action ->
+                    if (action == Action.NO_ACTION) {
+                        navigateToListScreen(action)
+                    } else {
+                        if (sharedViewModel.validateFields()) {
+                            navigateToListScreen(action)
+                        } else {
+                            displayToast(context)
+                        }
+                    }
+                }
             )
         },
         content = { paddingValues ->
@@ -52,4 +68,13 @@ fun TaskScreen(
 
         }
     )
+}
+
+
+fun displayToast(context: Context) {
+    Toast.makeText(
+        context,
+        context.getString(R.string.validation_toast),
+        Toast.LENGTH_SHORT
+    ).show()
 }
