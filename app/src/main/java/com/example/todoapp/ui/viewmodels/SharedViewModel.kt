adaa32576c88bfa13620f1c1846dc5c439d99265
Ben Haas <ben@benhaas.io>
2024-08-12 14:36:41 -0700

    add datastore for priority sorting

--- app/src/main/java/com/example/todoapp/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/viewmodels/SharedViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.todoapp.data.models.Priority
 import com.example.todoapp.data.models.ToDoTask
+import com.example.todoapp.data.repositories.DataStoreRepository
 import com.example.todoapp.data.repositories.ToDoRepository
 import com.example.todoapp.util.Action
 import com.example.todoapp.util.Constants.MAX_TITLE_LENGTH
@@ -23,7 +24,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SharedViewModel @Inject constructor(
-    private val repository: ToDoRepository
+    private val repository: ToDoRepository,
+    private val dataStoreRepository: DataStoreRepository
 ): ViewModel() {
 
     val action: MutableState<Action> = mutableStateOf(Action.NO_ACTION)
